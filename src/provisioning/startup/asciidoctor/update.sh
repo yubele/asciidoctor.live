93c7f554a8ab0e9d12252475338b049a4d6acb7a
Yusuke Abe <yusuke@newsdict.jp>
2020-04-28 02:04:04 +0000

    Update documents

--- src/provisioning/startup/asciidoctor/update.sh
+++ src/provisioning/startup/asciidoctor/update.sh
@@ -4,4 +4,7 @@ if [ "$(echo $2 | grep -e "index$" 2>&1 >/dev/null; echo $?)" = "0" ]; then
 else
     output=$2/index.html
 fi
-bundle exec asciidoctor --safe-mode unsafe -a lang=ja -b html5 -d book -r asciidoctor-diagram  $1 -o $output
+bundle exec asciidoctor --safe-mode unsafe \
+    -r asciidoctor-diagram \
+    -a stylesheet=../../asciidoctor-skins/css/clean.css \
+    -a lang=ja -b html5 -d book  $1 -o $output
